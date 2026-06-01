41a6f0d7801127af2bff4de0c3b6440759420d5e
heyez2333 <3557393676ayz@gmail.com>
2026-06-01 13:25:53 +0800

    Update cx.py

--- cx.py
+++ cx.py
@@ -222,9 +222,9 @@ class AutoSign(object):
 		params = {
 			'name': '',
 			'activeId': activeId,
-			'address': '中国',
+			'address': '南宁',
 			'uid': '',
-			'clientip': '0.0.0.0',
+			'clientip': '223.160.233.62',
 			'latitude': '-2',
 			'longitude': '-1',
 			'fid': '',
